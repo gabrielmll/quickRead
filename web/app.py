6520fc835b30065d4f700ed229e45fb983dfe333
Gabriel Mello <gabrielmll@gmail.com>
2024-05-11 17:37:18 -0300

    Translating some vars and files to english. VERSION is STABLE

--- web/app.py
+++ web/app.py
@@ -22,21 +22,20 @@ frases = [
 
 @app.route("/")
 def home():
-    palavra_aleatoria = random.choice(palavras)
-    challenge = Challenge(palavra_aleatoria, palavra_aleatoria)
-    print(challenge.get_challenge())
+    challenge_answer = random.choice(palavras)
+    challenge = Challenge(challenge_answer, challenge_answer)
     return render_template("index.html", challenge_view=challenge.get_challenge())
 
-@app.route("/resultado", methods=["POST"])
-def resultado():
+@app.route("/result", methods=["POST"])
+def result():
     user_answer = request.form["user-answer"]
     challenge_answer = eval(request.form["challenge-answer"])[1]
     ic(user_answer, challenge_answer)
 
     if user_answer == challenge_answer:
-        return render_template("resultado.html", acerto=True)
+        return render_template("result.html", acerto=True)
     else:
-        return render_template("resultado.html", acerto=False, challenge_view=challenge_answer)
+        return render_template("result.html", acerto=False, challenge_view=challenge_answer)
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
